f4ae25d4a2451b0828b1f465846c43d7c2219265
blikpatrik <blikpatrik@gmail.com>
2016-10-19 17:53:23 +0200

    100Doors finished

--- 100Doors.py
+++ 100Doors.py
@@ -1,20 +1,23 @@
 doors=['O']*100
 skip=0
 while(skip<100):
-  door=0
+  door=skip
+  print(door+1,end='. run:\n')
   while(door<100):
-    if doors[door]=='O':
+    if(doors[door]=='O'):
       doors[door]='X'
     else:
       doors[door]='O'
-    for missed in range(door-skip,skip):
-      print(doors[missed],end='')
+    missed=door-skip
+    while(missed<door):
+      print(' ',end='')
+      missed=missed+1
     print(doors[door],end='')
     door=door+skip+1
   print('\n')
   skip=skip+1
-print('The following doors are left open: ',end='')
+
+print('Above you see the doors toggled each run. \nThe following doors remained open: ',end='')
 for door in [door for door,open in enumerate(doors) if open=='O']:
-  print(',',end='')
-  print(door+1,end='')
+  print(door+1,end='. ')
 print('\n')
