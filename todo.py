31f00893b8f4dc0cb1849ce94d492d7232ca2f6a
blikpatrik <blikpatrik@gmail.com>
2016-10-19 18:41:00 +0200

    todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,10 @@
-print('please specify a command')
+todo=['python game','write thesis']
+print("Hi, I'm your Python assistant. How can I help?\n")
+task=input("1. What's today? \n2. Remember something. \n3. Forget something. \n4. Highlight something.\n")
+task=eval(task)
+if(task==1):
+  enumerate(todo)
+if(task==2):
+  todo.append(input("Sure, what's in plan? \n"))
+if(task==3):
+  todo.remove(input("OK, what's complete?"))
