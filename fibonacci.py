3a09891f6457368aa8fb29931cf474a077a26f18
blikpatrik <blikpatrik@gmail.com>
2016-10-19 09:29:11 +0200

    fibonacci done

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,14 @@
 print('fibonacci sequence: ') 
-length=input('how long should i count? ') 
-print(penultimate=0) 
-print(ultimate=1)
-for length in range(0,length)
+length=int(input('how long should i count? '))
+rank=1
+penultimate=0
+print(str(rank)+'.'+' '*19+str(penultimate))
+ultimate=1
+rank=rank+1
+print(str(rank)+'.'+' '*19+str(ultimate))
+for length in range(0,length):
   current=penultimate+ultimate
-  print(current)
+  rank=rank+1
+  print(str(rank)+'.'+' '*(21-len(str(rank))-len(str(current)))+str(current))
   penultimate=ultimate
   ultimate=current
